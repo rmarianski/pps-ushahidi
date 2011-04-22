2cdff26154b3ec879d1cfce364876be8021e51c1
Robert Marianski <rob@marianski.com>
2011-04-21 20:01:33 -0400

    generate site agnostic url for starburst

--- themes/pps/views/header.php
+++ themes/pps/views/header.php
@@ -51,7 +51,7 @@
 			<div id="logo">
 				<span><img src="<?php echo url::site('themes/pps/images/btc-ftc.png'); ?>" />
 				<?php echo $site_tagline; ?></span>
-			</div><a href="http://www.urbandesignweek.org/by-the-city/reports/submit"><span>http://www.urbandesignweek.org/by-the-city/reports/submit</span></a>
+   </div><a href="<?php echo url::site('reports/submit'); ?>"><span><?php echo url::site('reports/submit'); ?></span></a>
 			<!-- / logo -->
 
 			<!-- submit incident -->
