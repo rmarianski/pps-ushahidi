1d6ceae0b0af5a71194aa056ce178ed3648a0590
Robert Marianski <rob@marianski.com>
2011-05-09 20:53:38 -0500

    style updates from josh

--- themes/pps/views/search.php
+++ themes/pps/views/search.php
@@ -2,6 +2,11 @@
 	<div class="content-bg">
 		<!-- start search block -->
 		<div class="big-block">
+        <div class="search-box">
+   <form method="get" action="<?php echo url::site('search'); ?>">
+     <input id="ideas-search" type="text" name="k" value="Search" />
+   </form>
+</div>
 			<h1>Search Results</h1>
 			<div class="search_block">
 				<?php echo $search_info; ?>
